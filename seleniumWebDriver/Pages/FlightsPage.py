e8e410b9d2e7645c881a29741473db7e599b705e
Shivam Ojha <shivamojha999@gmail.com>
2023-12-05 17:43:38 +0530

    Changes: Added New TC for Flight Page 1)Created methods on flightpage

--- seleniumWebDriver/Pages/FlightsPage.py
+++ seleniumWebDriver/Pages/FlightsPage.py
@@ -1,5 +1,8 @@
+import re
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from matplotlib.colors import to_hex
 
 from seleniumWebDriver.Pages.CommonPage import CommonPage
 
@@ -9,6 +12,10 @@ class FlightsPage(CommonPage):
     lblFlights = ".//*[@id='root']"
     lblHeaderOptions = ".//header[contains(@class,'Header-module')]"
     lblAdultPopup = ".//*[contains(@data-ui-name,'occupancy')]//label"
+    lstItems = ".//div[@data-ui-name='segments_list_item']"
+    txtSearchLocation = ".//input[@placeholder='Airport or city']"
+    lstSearchOptions = ".//*[@id='flights-searchbox_suggestions']"
+    lblSearchResults = ".//div[@data-testid='searchresults_list']"
 
     '''
     created By: Shivam Ojha
@@ -126,3 +133,97 @@ class FlightsPage(CommonPage):
         self.waitUntilPageRefreshed()
         self.waitUntilPageReady(self.lblAdultPopup)
         return self.driver.find_element(By.XPATH,self.lblAdultPopup+"[contains(text(),'"+optionName+"')]/parent::div/parent::div//span[contains(@class,'InputStepper')]").text
+
+    '''
+    created By: Shivam Ojha
+    since: 20 Nov 2023
+    desc: This method is used to verify Color Of Specific Button In Flight Page
+    param: button,color
+    return: boolean
+    '''
+    def verifyColorOfSpecificButtonInFlightPage(self,button,color):
+        self.waitUntilPageRefreshed()
+        self.waitUntilPageReady(self.lblFlights)
+        rgba_string = self.driver.find_element(By.XPATH,self.lblFlights+"//button/span[contains(text(),'"+button+"')]").value_of_css_property('color')
+        rgba_values=[]
+        for value in re.findall(r'\d+\.\d+|\d+', rgba_string):
+            rgba_values.append(float(value)/255.0)
+        return to_hex(rgba_values[:-1]).__eq__(color)
+
+    '''
+    created By: Shivam Ojha
+    since: 05 Dec 2023
+    desc: This method is used to click On Specific From To Text Box
+    param: optionFromTo
+    return: none
+    '''
+    def clickOnSpecificFromToTextBox(self,optionFromTo):
+        self.waitUntilPageRefreshed()
+        self.waitUntilPageReady(self.lstItems)
+        self.driver.find_element(By.XPATH,self.lstItems+"//button[contains(@data-ui-name,'input_location_"+optionFromTo.lower()+"_segment')]").click()
+
+    '''
+    created By: Shivam Ojha
+    since: 05 Dec 2023
+    desc: This method is used to click On Cross Icon For Specific Location
+    param: locationName
+    return: none
+    '''
+    def clickOnCrossIconForSpecificLocation(self,locationName):
+        self.waitUntilPageRefreshed()
+        self.waitUntilPageReady(self.txtSearchLocation+"/parent::div")
+        self.driver.find_element(By.XPATH,self.txtSearchLocation+"/parent::div/span/span[contains(text(),"+locationName+")]/following-sibling::span").click()
+
+    '''
+    created By: Shivam Ojha
+    since: 05 Dec 2023
+    desc: This method is used to search Location
+    param: searchOption
+    return: none
+    '''
+    def searchLocation(self,searchOption):
+        self.waitUntilPageRefreshed()
+        self.waitUntilPageReady(self.txtSearchLocation+"/parent::div")
+        self.driver.find_element(By.XPATH,self.txtSearchLocation).send_keys(searchOption)
+
+    '''
+    created By: Shivam Ojha
+    since: 05 Dec 2023
+    desc: This method is used to select Location
+    param: locationName
+    return: none
+    '''
+    def selectLocation(self,locationName):
+        self.waitUntilPageRefreshed()
+        self.waitUntilPageReady(self.lstSearchOptions)
+        self.driver.find_element(By.XPATH,self.lstSearchOptions+"//li//b[text()='"+locationName+"']").click()
+
+    '''
+    created By: Shivam Ojha
+    since: 05 Dec 2023
+    desc: This method is used to search and select Location
+    param: optionFromTo,searchOption
+    return: none
+    '''
+    def searchAndSelectSpecificLocation(self,optionFromTo,searchOption):
+        self.waitUntilPageRefreshed()
+        self.waitUntilPageReady(self.lstItems)
+        self.clickOnSpecificFromToTextBox(optionFromTo)
+        if len(self.driver.find_elements(By.XPATH,self.txtSearchLocation+"/parent::div/span")) > 0:
+            for i in range(len(self.driver.find_elements(By.XPATH,self.txtSearchLocation+"/parent::div/span"))):
+                location = self.driver.find_element(By.XPATH,self.txtSearchLocation+"/parent::div/span["+str(i+1)+"]/span/b").text
+                self.clickOnCrossIconForSpecificLocation(location)
+        self.searchLocation(searchOption)
+        self.selectLocation(searchOption)
+
+    '''
+    created By: Shivam Ojha
+    since: 05 Dec 2023
+    desc: This method is used to verify search results displayed
+    param: none
+    return: boolean
+    '''
+    def verifySearchResultsDsplayed(self):
+        self.waitUntilPageRefreshed()
+        self.waitUntilPageReady(self.lblSearchResults)
+        return self.driver.find_element(By.XPATH,self.lblSearchResults).is_displayed()
